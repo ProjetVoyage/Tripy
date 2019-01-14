8e47dbb4c2a4237878f7efe3e3116637ff7b0347
Lancelot Demay <lde@frianbiz.com>
2019-01-14 20:16:44 +0100

    Changed user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,14 +2,13 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -19,24 +18,20 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="json")
      */
-    private $password;
+    private $roles = [];
 
     /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Traveler", mappedBy="user_id")
+     * @var string The hashed password
+     * @ORM\Column(type="string")
      */
-    private $travelers;
-
-    public function __construct()
-    {
-        $this->travelers = new ArrayCollection();
-    }
+    private $password;
 
     public function getId(): ?int
     {
@@ -55,9 +50,41 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->password;
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
     }
 
     public function setPassword(string $password): self
@@ -68,33 +95,19 @@ class User
     }
 
     /**
-     * @return Collection|Traveler[]
+     * @see UserInterface
      */
-    public function getTravelers(): Collection
+    public function getSalt()
     {
-        return $this->travelers;
+        // not needed when using the "bcrypt" algorithm in security.yaml
     }
 
-    public function addTraveler(Traveler $traveler): self
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
     {
-        if (!$this->travelers->contains($traveler)) {
-            $this->travelers[] = $traveler;
-            $traveler->setUserId($this);
-        }
-
-        return $this;
-    }
-
-    public function removeTraveler(Traveler $traveler): self
-    {
-        if ($this->travelers->contains($traveler)) {
-            $this->travelers->removeElement($traveler);
-            // set the owning side to null (unless already changed)
-            if ($traveler->getUserId() === $this) {
-                $traveler->setUserId(null);
-            }
-        }
-
-        return $this;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
