fa15080fe30606989e357127d7558b6ebd5ec966
Albert Krasniqi <albert.krasniqi@groupetrefle.com>
2019-02-28 12:43:33 +0100

    pb non réglé

--- src/Form/ExpenseType.php
+++ src/Form/ExpenseType.php
@@ -8,15 +8,18 @@ use App\Transformers\DateTimeTransformer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\CallbackTransformer;
 
 class ExpenseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date', DateType::class, [
+            ->add('date', DateType::class, [ // J'ai laisser en DateTime pour que le traitement fonctionne
+                                            // Si TexteType, décommenter widget et html5
                 'widget' => 'single_text',
                 'html5' => false,
                 'attr' => ['class' => 'js-datepicker'],
@@ -27,9 +30,9 @@ class ExpenseType extends AbstractType
                 'class' => Traveler::class,
                 'choice_label' => 'username'
             ]);
-
-        $builder->get('date')
-            ->addModelTransformer(new DateTimeTransformer());
+            
+        // $builder->get('date')
+        //     ->addModelTransformer(new DateTimeTransformer());
     }
 
     public function configureOptions(OptionsResolver $resolver)
