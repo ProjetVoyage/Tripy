12c82e882674c410c60ff9da02ed23d9e31ac76b
Albert Krasniqi <albert.krasniqi@groupetrefle.com>
2019-02-28 12:43:33 +0100

    pb non réglé

--- src/Transformers/DateTimeTransformer.php
+++ src/Transformers/DateTimeTransformer.php
@@ -16,8 +16,31 @@ class DateTimeTransformer implements DataTransformerInterface
         if (!is_object($datetime)) {
             throw new TransformationFailedException('Expected a datetime.');
         }
+        /*
+        $date = \DateTime::createFromFormat('Y/d/m', '2017/01/01');
+        print_r($date);
+        die();
 
+        $maStr = $datetime->format('d/m/Y'); // date début est le nom de mon input
+        $zedate = \DateTime::createFromFormat('d/m/Y', $maStr);
+return $zedate;
+        print_r($maStr);
+        print_r($zedate);
+        die();
+        $datetime = new \DateTime();
+        $pp = $datetime->format('d/m/Y');
+
+        print_r($pp);
+        print_r($datetime);
+        
+        $date = new \DateTime();
+        $d  = $date->createFromFormat('Y-m-d', $datetime);
+        echo $d->format('Y-m-d');
+die();
+        // print_r($datetime);die();*/
         return $datetime->format('d/m/Y H:i');
+        
+        // return $datetime;
     }
 
     public function reverseTransform($stringDate)
@@ -25,11 +48,16 @@ class DateTimeTransformer implements DataTransformerInterface
         if (null === $stringDate) {
             return NULL;
         }
-
+        
         if (!is_string($stringDate)) {
             throw new TransformationFailedException('Expected a string.');
         }
 
+        $date = new \DateTime();
+        // print_r($date->createFromFormat('d/m/Y', $stringDate)); Pour une raison que j'ignore il ne reconnait pas la date envoyé
+        // print_r($date->createFromFormat('d/m/Y', '15/02/2019'));
+        // print_r($date->createFromFormat('d/m/Y', '10/02/2019'));die();
+        
         return \DateTime::createFromFormat('d/m/Y', $stringDate);
     }
 }
